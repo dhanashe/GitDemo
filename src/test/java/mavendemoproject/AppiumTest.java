2d76ff19683b39540d1507e5fbbffd6e0e441dec
dhanashe <saranyadsn@gmail.com>
2019-04-29 12:30:41 -0400

    first commit

---
+++ src/test/java/mavendemoproject/AppiumTest.java
@@ -0,0 +1,18 @@
+package mavendemoproject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test 
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	@Test 
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/mavendemoproject/SeleniumTest.java
@@ -0,0 +1,17 @@
+package mavendemoproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test 
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test 
+	public void ElementUI()
+	{
+		System.out.println("ElementUI");
+	}
+
+}
